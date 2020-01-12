9b98255fd0c81d8e0ad07821ab08f7b63350a99f
Cain <caintaylor2015@gmail.com>
2020-01-12 17:29:31 -0600

    Restaurant Assessment Finalized.

--- Restaurant/scripts/confirmValidate.js
+++
@@ -1,9 +0,0 @@
-
-function confirmValidate() {
-    if (form.checkValidity() === true) {
-        alert("Your information is valid!");
-    }
-    else {
-        alert("Your invofrmation is not valid!");
-    }
-}
